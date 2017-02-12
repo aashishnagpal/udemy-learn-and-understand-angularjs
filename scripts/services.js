fbb10b0108771f8839d90d063b9ecb514092e732
Aashish Nagpal <aashish.nagpal@hotmail.com>
2017-02-12 13:09:59 +0530

    Update services.js

--- scripts/services.js
+++ scripts/services.js
@@ -11,7 +11,7 @@ app.factory('weatherFactory', function ($resource) {
 
   var API_PATH = 'http://api.openweathermap.org/data/2.5/forecast/daily';
   var APP_ID = '089c7f3f5a9253c4eadba886da4e4f5c';
-  var weatherResource = $resource(API_PATH, {appid: APP_ID, callback: "JSON_CALLBACK"}, { get: { method: "JSONP" }});
+  var weatherResource = $resource(API_PATH, { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 
   return {
     getWeather: function (days, city, latitude, longitude) {
@@ -23,6 +23,7 @@ app.factory('weatherFactory', function ($resource) {
         weatherParams.lon = longitude;
       }
       weatherParams.cnt = days;
+      weatherParams.appid = APP_ID;
 
       return weatherResource.get(weatherParams, function (successResult) {
         return successResult;
